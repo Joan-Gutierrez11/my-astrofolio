69811c508c3e42dc21bdf8fcd85e34ec05691f51
Joan-Gutierrez11 <joangutierrez2001@gmail.com>
2026-07-05 19:58:18 -0500

    Revert "fix: update environment variables and improve deployment configuration"
    This reverts commit ba34fc90da2c584ef21d6575e76ce301ae266bf4.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,10 @@
 import { defineConfig } from 'astro/config';
 import vue from '@astrojs/vue';
 import tailwindcss from "@tailwindcss/vite";
-import dotenv from 'dotenv';
 
-// Load .env file contents into process.env
-dotenv.config();
-
-const DEV_MODE = process.env.DEV_MODE === 'true';
-
-/**
- * @type {import('astro').AstroUserConfig}
- */
-const baseConfig = {
+export default defineConfig({
+    site: 'https://Joan-Gutierrez11.github.io',
+    base: '/my-astrofolio',
     integrations: [vue()],
     vite: {
         plugins: [tailwindcss()]
@@ -23,23 +16,4 @@ const baseConfig = {
             prefixDefaultLocale: true,
         }
     }
-};
-
-/**
- * @type {Pick<import('astro').AstroUserConfig, 'site' | 'base'>}
- */
-const webConfig = {
-    site: process.env.BASE_SITE,
-    base: process.env.BASE_WEB,
-}
-
-/**
- * Initialize config according to the DEV_MODE environment variable.
- * 
- * @type {import('astro').AstroUserConfig}
- */
-const config = DEV_MODE ? baseConfig : { ...baseConfig, ...webConfig };
-
-console.log("Env: ", config);
-
-export default defineConfig(config);
+});
